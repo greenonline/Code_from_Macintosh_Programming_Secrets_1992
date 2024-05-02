5686a13d6bca495ac0856c94897c8627a26ace20
greenonline <greenonline@users.noreply.github.com>
2024-05-02 09:17:29 +0100

    Create Progress_indicator.h

---
+++ Chapter4/Progress_indicator.h
@@ -0,0 +1,13 @@
+/* Listing 4-3 Progress Indicator.h */
+
+/* Progress indicator routines */
+
+void InitProgressIndicator (GrafPtr ourPort, Rect r, long max);
+void SetProgress(long absoluteAmount);
+Boolean SetProgressDelta(long delta);
+void DrawProgressBar(void);
+void UpdateProgressBar(void);
+
+void SetFrameColor(void);
+void SetDoneColor(void);
+void SetToDoColor(void);
