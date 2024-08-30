e0f09b08e47bdd67d5f947615bb71686b4ea2bf9
greenonline <greenonline@users.noreply.github.com>
2024-08-30 11:14:24 +0100

    Add files via upload

---
+++ with_LF/Chapter3/Standard_StuffPlus.r
@@ -0,0 +1,92 @@
+/* StandardStuffPlus.h.                                */
+/* Taken from Creation.r from Programmers Guide to MPW */
+/* Has additional Select All in Edit menu              */
+
+#include "Types.r"; 
+#include "SysTypes.r" 
+#include "StandardStuff.h"
+
+resource 'MBAR' (rMenuBar, preload) {
+    { mApple, mFile, mEdit };
+};
+
+resource 'MENU' (mApple, preload) {
+    mApple, textMenuProc,
+    0b1111111111111111111111111111101, /* disable dashed line, */enabled, apple,/* enable About and DAs*/
+    {
+        "About...",
+            noicon, nokey, nomark, plain;
+        "-",
+            noicon, nokey, nomark, plain
+    }
+};
+
+resource 'MENU' (mFile, preload) {
+    mFile, textMenuProc, 0b1111111111111111111101100001000, enabled, "File",
+    {
+        "New",
+            noicon, "N", nomark, plain;
+        "Open",
+            noicon, "O", nomark, plain;
+        "-",
+            noicon, nokey, nomark, plain; 
+        "Close",
+            noicon, "W", nomark, plain; 
+        "Save",
+            noicon, "S", nomark, plain; 
+        "Save As...",
+            noicon, nokey, nomark, plain; 
+        "Revert",
+            noicon, nokey, nomark, plain;
+        "-",
+            noicon, nokey, nomark, plain; 
+        "Page Setup...",
+            noicon, nokey, nomark, plain; 
+        "Print...",
+            noicon, nokey, nomark, plain;
+        "-",
+            noicon, nokey, nomark, plain; 
+        "Quit",
+            noicon, "Q", nomark, plain
+    }
+};
+
+resource 'MENU' (mEdit, preload) {
+
+mEdit, textMenuProc, 0b1111111111111111111111110111101, enabled, "Edit",
+    {
+        "Undo",
+            noicon, "Z", nomark, plain;
+        "-",
+            noicon, nokey, nomark, plain; 
+        "Cut",
+            noicon, "X", nomark, plain;
+
+        "Copy",
+            noicon, "C", nomark, plain;
+        "Paste",
+            noicon, "V", nomark, plain;
+        "Clear",
+            noicon, "B", nomark, plain;
+        "-",
+            noicon, nokey, nomark, plain; 
+        "Select All",
+            noicon, "A", nomark, plain
+    }
+};
+
+
+resource 'SIZE' (-1) { /* MultiFinder-aware application */
+    dontSaveScreen,
+    acceptSuspendResumeEvents,
+    enableOptionSwitch,
+    canBackground,
+    multiFinderAware,
+    backgroundAndForeground,
+    dontGetFrontClicks,
+    ignoreChildDiedEvents,
+    not32BitCompatible,
+    reserved, reserved, reserved, reserved, reserved, reserved, reserved, 
+    96*1024,
+    64*1024
+};
